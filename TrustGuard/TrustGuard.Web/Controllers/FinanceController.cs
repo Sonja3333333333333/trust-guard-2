153a9440450d8c78666dbe83b81281b71a33e219
Sonja3333333333333 <SOFIIA.BOHDANOVYCH@lnu.edu.ua>
2026-03-21 13:32:48 +0200

    added empty controllers and views

---
+++ TrustGuard/TrustGuard.Web/Controllers/FinanceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrustGuard.Web.Controllers
+{
+    public class FinanceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TrustGuard/TrustGuard.Web/Controllers/NewsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrustGuard.Web.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
