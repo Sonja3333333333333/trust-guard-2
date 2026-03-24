33be43daf06af38fd5a6b05830f842eac43f24b0
Sonja3333333333333 <SOFIIA.BOHDANOVYCH@lnu.edu.ua>
2026-03-24 12:46:46 +0200

    added history to dashboard

--- TrustGuard/TrustGuard.Application/Interfaces/INewsCheckRepository.cs
+++ TrustGuard/TrustGuard.Application/Interfaces/INewsCheckRepository.cs
@@ -5,5 +5,9 @@ namespace TrustGuard.Application.Interfaces
     public interface INewsCheckRepository
     {
         Task AddAsync(NewsCheck newsCheck);
+
+        Task<List<NewsCheck>> GetUserHistoryAsync(string userId);
+
+        Task<NewsCheck?> GetByIdAsync(int id);
     }
 }
--- TrustGuard/TrustGuard.Application/Interfaces/INewsCheckService.cs
+++ TrustGuard/TrustGuard.Application/Interfaces/INewsCheckService.cs
@@ -1,9 +1,14 @@
-﻿using TrustGuard.Domain.Entities;
+﻿using TrustGuard.Application.Models;
+using TrustGuard.Domain.Entities;
 
 namespace TrustGuard.Application.Interfaces
 {
     public interface INewsCheckService
     {
         Task SaveCheckResultAsync(string userId, string content, string verdict, float confidence);
+        Task<List<NewsCheck>> GetRecentUserHistoryAsync(string userId);
+        Task<List<NewsCheck>> GetFullUserHistoryAsync(string userId);
+        Task<DashboardStatsDto> GetDashboardStatsAsync(string userId);
+        Task<NewsCheck?> GetCheckDetailsAsync(int id, string userId);
     }
 }
