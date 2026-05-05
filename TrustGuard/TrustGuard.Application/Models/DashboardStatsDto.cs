9a03a49d5c7fde04d2bca5debb3e582f9b60eedc
sofia <SOFIIA.BOHDANOVYCH@lnu.edu.ua>
2026-05-05 09:29:30 +0300

    Додала графік на дошборд

--- TrustGuard/TrustGuard.Application/Models/DashboardStatsDto.cs
+++ TrustGuard/TrustGuard.Application/Models/DashboardStatsDto.cs
@@ -8,5 +8,8 @@ namespace TrustGuard.Application.Models
         public double FakePercentage { get; set; }
         public double SafetyScore { get; set; }
         public List<NewsCheck> RecentHistory { get; set; } = new();
+        public List<string> ChartLabels { get; set; } = new List<string>();
+        public List<int> ChartRealData { get; set; } = new List<int>();
+        public List<int> ChartFakeData { get; set; } = new List<int>();
     }
 }
